866fdbd87a056bd1e75b911a41399baa1b4e1bf3
sfreiman1 <scott@qwire.com>
2021-04-06 18:01:43 -0400

    Added additional functionality as outlined in README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Pac-Man-Exercise
 This repository contains the Pac Man Exercise from the MIT MERN course.
+
+# Overview
+
+This program allows a user to create between one and 100 pacmen and place them at a random position on a gameboard. When the game is started the pacman move at a random velocity until they hit one of the gameboard's borders.
+
+# Original assignment
+
+* Create a button to allow user to create a Pac Man at a random position and velocity
+* Create a button to start all Pac Men moving across the screen
+* When a Pac Man hits a border, have it reverse direction 
+
+# Additional modifications
+
+* Added Title
+* Added Clear Pac Man button
+* Added Stop button
+* Added Create Multiple button
+* Defined "game board" and allowed for dynamic resizing
+* Flipped direction of Pacmen when they hit the edge
+* Styled buttons and background
+
+# Buttons
+
+* Start Game -- Causes any pacmen on the gameboard to begin moving. Each pacman moves at a random velocity.
+* Stop Game -- Causes any pacmen on the gameboard to stop moving.
+* Add PacMan -- Adds a PacMan to the game board at a random position.
+* Create Multiple -- Prompts the user for a number between 1 and 100 and creates that number of PacMen on the game board.
+* Clear PacMen -- Removes all pacmen from the gameboard.
--- pacmen.js
+++ pacmen.js
@@ -1,12 +1,39 @@
-let pos = 0;
+// Pacmen.js
+
+// Pacman images
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ["./images/PacMan1.png", "./images/PacMan2.png"],
+  ["./images/PacMan3.png", "./images/PacMan4.png"],
 ];
-let direction = 0;
+
 const pacMen = []; // This array holds all the pacmen
 
-// This function returns an object with random values
+let stopMovement = false; // stop pacmen moving
+let gameRunning = false; // is the game still running?
+
+// store the game board dimensions so we don't have to keep looking them up
+let gameBoardHeight, gameBoardWidth;
+
+// initializes the game board
+window.onload = function initBoard() {
+  resizeBoard(window.innerHeight - 200, window.innerWidth - 50);
+};
+
+// resizes the game board
+function resizeBoard(h, w) {
+  let elem = document.getElementById("game");
+
+  elem.style.height = h;
+  elem.style.width = w;
+  elem.style.border = "thick solid #000000";
+  elem.style.position = "fixed";
+  elem.style.backgroundColor = "blue";
+
+  gameBoardWidth = elem.clientWidth;
+  gameBoardHeight = elem.clientHeight;
+}
+
+// returns an object with random values
 function setToRandom(scale) {
   return {
     x: Math.random() * scale,
@@ -14,24 +41,33 @@ function setToRandom(scale) {
   };
 }
 
-// Factory to make a PacMan at a random position with random velocity
+// makes a PacMan at a random position with random velocity
 function makePac() {
+  // make sure we are ready to start movement again
+  stopMovement = false;
+
   // returns an object with random values scaled {x: 33, y: 21}
-  let velocity = setToRandom(10); // {x:?, y:?}
-  let position = setToRandom(200);
+  let velocity = setToRandom(50); // {x:?, y:?}
+  let position = {};
+  let direction = 0; // 0 = left to right, 1 = right to left
+  let focus = 0; // Toggle among PacMan images
 
   // Add image to div id = game
-  let game = document.getElementById('game');
-  let newimg = document.createElement('img');
-  newimg.style.position = 'absolute';
+  let game = document.getElementById("game");
+  let newimg = document.createElement("img");
+  newimg.style.position = "absolute";
   newimg.src = pacArray[0][0];
   newimg.width = 100;
+  newimg.height = 100;
 
-  // TODO: set position here
+  // set position here
+  position.x = Math.random() * (gameBoardWidth - newimg.width);
+  position.y = Math.random() * (gameBoardHeight - newimg.height);
+
   newimg.style.left = position.x;
   newimg.style.top = position.y;
 
-  // TODO add new Child image to game
+  // Tadd new Child image to game
   game.appendChild(newimg);
 
   // return details in an object
@@ -39,13 +75,37 @@ function makePac() {
     position,
     velocity,
     newimg,
+    direction,
+    focus,
   };
 }
 
+//  toggles the state of the start and stop buttons
+function toggleGameButtons() {
+  startBut = document.getElementById("start");
+  startBut.disabled = !startBut.disabled;
+
+  stopBut = document.getElementById("stop");
+  stopBut.disabled = !stopBut.disabled;
+}
+
+//  updates the position of all the pacmen
 function update() {
   // loop over pacmen array and move each one and move image in DOM
+
+  if (stopMovement === true) {
+    stopMovement = false;
+    gameRunning = false;
+    return;
+  }
+  gameRunning = true;
+
   pacMen.forEach((item) => {
     checkCollisions(item);
+
+    item.newimg.src = pacArray[item.direction][item.focus];
+    item.focus = (item.focus + 1) % 2;
+
     item.position.x += item.velocity.x;
     item.position.y += item.velocity.y;
 
@@ -55,23 +115,67 @@ function update() {
   setTimeout(update, 20);
 }
 
+//  checks to see if the pacmen has collided with one of the four sides.
 function checkCollisions(item) {
-  // TODO: detect collision with all walls and make pacman bounce
-  if ((item.position.x + item.velocity.x + item.newimg.width > window.innerWidth) ||
-     (item.position.x + item.velocity.x < 0)) {
+  if (
+    item.position.x + item.velocity.x + item.newimg.width > gameBoardWidth ||
+    item.position.x + item.velocity.x < 0
+  ) {
     item.velocity.x = -item.velocity.x;
+    item.direction = (item.direction + 1) % 2;
   }
-  if ((item.position.y + item.velocity.y + item.newimg.height > window.innerHeight) ||
-      (item.position.y + item.velocity.y < 0)){
+  if (
+    item.position.y + item.velocity.y + item.newimg.height > gameBoardHeight ||
+    item.position.y + item.velocity.y < 0
+  ) {
     item.velocity.y = -item.velocity.y;
   }
 }
 
+//  makes a single pacman
 function makeOne() {
   pacMen.push(makePac()); // add a new PacMan
 }
 
-//don't change this line
-if (typeof module !== 'undefined') {
-  module.exports = { checkCollisions, update, pacMen };
+//  clear all pacmans from the gamespace
+function clearAll() {
+  let images = document.getElementsByTagName("img");
+
+  // If game is running, stop it
+  if (gameRunning) stop();
+
+  // Since removing nodes continually updates the node list, we have to keep removing the first node until there isn't one
+  for (let i = 0, len = images.length; i != len; ++i) {
+    images[0].parentNode.removeChild(images[0]);
+  }
+
+  //clear the array
+  pacMen.length = 0;
+}
+
+// starts the pacmen moving
+function start() {
+  toggleGameButtons();
+  update();
+}
+
+// stops the pacmen moving
+function stop() {
+  toggleGameButtons();
+  gameRunning = false;
+  stopMovement = true;
+}
+
+// creates one or more pacmen based on the number entered by the user
+function createMultiple() {
+  var num = parseInt(prompt("How many Pacmen do you want?"));
+  if (num === NaN) {
+    alert("You must enter an integer.");
+  } else if (num <= 0 || num > 100) {
+    alert("You must enter a number between 1 and 100");
+  } else {
+    for (let i = 0; i < num; i++) {
+      makeOne();
+    }
+  }
 }
